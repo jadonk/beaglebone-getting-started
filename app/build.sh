1abe489380965450699fcb2e3e4eaf1df60d83b0
Jason Kridner <jdk@ti.com>
2012-11-17 00:10:35 -0500

    Added initial build for linux-x64

--- app/build.sh
+++ app/build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
+set -x
+set -e
 ZIP=/tmp/$(basename $0).$RANDOM.zip
-cd $(dirname $0)/../
+cd $(dirname $0)/..
+DIR=`pwd`
 
-rm app/getting-started
-zip $ZIP -r * -x app/*
+cd $DIR
+zip $ZIP -r autorun.inf Docs Drivers info.txt LICENSE.txt README.htm
 
-# Add node-webkit plugins
-cd app
-zip $ZIP package.json nw.pak libffmpegsumo.so plugins/*
-cd ..
+# Add package.json
+cd $DIR/app
+zip $ZIP package.json
 
-cat app/nw-linux-x64 $ZIP > app/getting-started-x64
-chmod +x app/getting-started-x64
+cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/getting-started-x64
+chmod +x $DIR/app/getting-started-x64
 
-cat app/nw-linux-x32 $ZIP > app/getting-started-x32
-chmod +x app/getting-started-x32
-
+# Clean-up
 rm $ZIP
