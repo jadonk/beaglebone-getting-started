3b7577a7c89608d5975eae89ea55784ada5281a5
Jason Kridner <jdk@ti.com>
2012-11-17 15:52:08 -0500

    Rename Linux executables and add ia32

--- app/build.sh
+++ app/build.sh
@@ -12,8 +12,11 @@ zip $ZIP -r autorun.inf Docs Drivers info.txt LICENSE.txt README.htm
 cd $DIR/app
 zip $ZIP package.json
 
-cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/getting-started-x64
-chmod +x $DIR/app/getting-started-x64
+cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/getting-started.x64
+chmod +x $DIR/app/getting-started.x64
+
+cat $DIR/app/node-webkit-latest-linux-ia32/nw $ZIP > $DIR/app/getting-started.ia32
+chmod +x $DIR/app/getting-started.ia32
 
 # Clean-up
 rm $ZIP
