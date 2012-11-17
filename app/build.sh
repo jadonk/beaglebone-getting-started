e5f284e6074f12a67b25ac4bb43f8cb27d89ef1f
Jason Kridner <jdk@ti.com>
2012-11-17 18:50:53 -0500

    Clean-up

--- app/build.sh
+++ app/build.sh
@@ -12,13 +12,18 @@ zip $ZIP -r autorun.inf Docs Drivers info.txt LICENSE.txt README.htm
 cd $DIR/app
 zip $ZIP package.json
 
+# Make Mac app
+rm -f $DIR/app/beaglebone-getting-started.app.zip
 cp $ZIP $DIR/app/beaglebone-getting-started.app/Contents/Resources/app.nw
+zip $DIR/app/beaglebone-getting-started.app.zip -r $DIR/app/beaglebone-getting-started.app
 
+# Make Linux 64-bit app
 cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/beaglebone-getting-started.x64
 chmod +x $DIR/app/beaglebone-getting-started.x64
 
+# Make Linux 32-bit app
 cat $DIR/app/node-webkit-latest-linux-ia32/nw $ZIP > $DIR/app/beaglebone-getting-started.ia32
 chmod +x $DIR/app/beaglebone-getting-started.ia32
 
 # Clean-up
-rm $ZIP
+rm -f $ZIP
