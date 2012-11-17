d8462deee85fde8d86e3c988ac5b8771cbafc6f2
Jason Kridner <jdk@ti.com>
2012-11-17 17:48:15 -0500

    Added Mac build

--- app/build.sh
+++ app/build.sh
@@ -12,11 +12,13 @@ zip $ZIP -r autorun.inf Docs Drivers info.txt LICENSE.txt README.htm
 cd $DIR/app
 zip $ZIP package.json
 
-cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/getting-started.x64
-chmod +x $DIR/app/getting-started.x64
+cp $ZIP $DIR/app/beaglebone-getting-started.app/Contents/Resources/app.nw
 
-cat $DIR/app/node-webkit-latest-linux-ia32/nw $ZIP > $DIR/app/getting-started.ia32
-chmod +x $DIR/app/getting-started.ia32
+cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/beaglebone-getting-started.x64
+chmod +x $DIR/app/beaglebone-getting-started.x64
+
+cat $DIR/app/node-webkit-latest-linux-ia32/nw $ZIP > $DIR/app/beaglebone-getting-started.ia32
+chmod +x $DIR/app/beaglebone-getting-started.ia32
 
 # Clean-up
 rm $ZIP
