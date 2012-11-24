d8f5e657a5949b0af52aeccac9c80c32bcd1ba9a
Jason Kridner <jdk@ti.com>
2012-11-24 00:50:42 -0500

    Added node-mdns

--- app/test.js
+++ app/test.js
@@ -7,6 +7,18 @@ var gotIP = function(error, ip) {
 funcs.getNetworkIPs(gotIP, false);
 
 var options = funcs.listLatestDownloads.options;
-funcs.listLatestDownloads.options.path = 'http://' + options.host + options.path;
-funcs.listLatestDownloads.options.host = 'wwwgate.ti.com';
+//funcs.listLatestDownloads.options.path = 'http://' + options.host + options.path;
+//funcs.listLatestDownloads.options.host = 'wwwgate.ti.com';
 funcs.listLatestDownloads();
+
+var mdns = require('mdns');
+var browser = mdns.createBrowser(mdns.tcp('ssh'));
+
+browser.on('serviceUp', function(service) {
+  console.log("service up: ", service);
+});
+browser.on('serviceDown', function(service) {
+  console.log("service down: ", service);
+});
+
+browser.start();
