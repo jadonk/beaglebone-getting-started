c036b0049df3018e7e13efc709f2fbbc5f0a8b80
Jason Kridner <jdk@ti.com>
2012-12-28 13:22:18 -0500

    app.js: added try/catch to simplify debugging

--- app/app.js
+++ app/app.js
@@ -1,36 +1,70 @@
-var http = require('http');
-var socketio = require('socket.io');
-var mdns = require('mdns');
-var funcs = require('./funcs');
+console.log('Starting app.js');
 
-//var io = socketio.listen(process.env.PORT || 3000);
-var io = socketio.listen(3000);
+try {
+ var http = require('http');
+ var socketio = require('socket.io');
+ var mdns = require('mdns');
+ var funcs = require('./funcs');
 
-var socketConnect = function(socket) {
- var mdnsServiceUp = function(service) {
-  socket.emit('serviceUp', service);
+ var socketConnect = function(socket) {
+  var mdnsServiceUp = function(service) {
+   socket.emit('serviceUp', service);
+  };
+  var mdnsServiceDown = function(service) {
+   socket.emit('serviceDown', service);
+  };
+  var mdnsError = function(error) {
+   console.log('mdns error: ' + error);
+  };
+  mdns.createBrowser(mdns.tcp('ssh'))
+   .on('serviceUp', mdnsServiceUp)
+   .on('serviceDown', mdnsServiceDown)
+   .on('error', mdnsError)
+   .start();
+  mdns.createBrowser(mdns.tcp('http'))
+   .on('serviceUp', mdnsServiceUp)
+   .on('serviceDown', mdnsServiceDown)
+   .on('error', mdnsError)
+   .start();
+  mdns.createBrowser(mdns.tcp('https'))
+   .on('serviceUp', mdnsServiceUp)
+   .on('serviceDown', mdnsServiceDown)
+   .on('error', mdnsError)
+   .start();
  };
- var mdnsServiceDown = function(service) {
-  socket.emit('serviceDown', service);
+
+ var socketConnect = function(socket) {
+  var mdnsServiceUp = function(service) {
+   socket.emit('serviceUp', service);
+  };
+  var mdnsServiceDown = function(service) {
+   socket.emit('serviceDown', service);
+  };
+  var mdnsError = function(error) {
+   console.log('mdns error: ' + error);
+  };
+  mdns.createBrowser(mdns.tcp('ssh'))
+   .on('serviceUp', mdnsServiceUp)
+   .on('serviceDown', mdnsServiceDown)
+   .on('error', mdnsError)
+   .start();
+  mdns.createBrowser(mdns.tcp('http'))
+   .on('serviceUp', mdnsServiceUp)
+   .on('serviceDown', mdnsServiceDown)
+   .on('error', mdnsError)
+   .start();
+  mdns.createBrowser(mdns.tcp('https'))
+   .on('serviceUp', mdnsServiceUp)
+   .on('serviceDown', mdnsServiceDown)
+   .on('error', mdnsError)
+   .start();
  };
- var mdnsError = function(error) {
-  console.log('mdns error: ' + error);
- };
- mdns.createBrowser(mdns.tcp('ssh'))
-  .on('serviceUp', mdnsServiceUp)
-  .on('serviceDown', mdnsServiceDown)
-  .on('error', mdnsError)
-  .start();
- mdns.createBrowser(mdns.tcp('http'))
-  .on('serviceUp', mdnsServiceUp)
-  .on('serviceDown', mdnsServiceDown)
-  .on('error', mdnsError)
-  .start();
- mdns.createBrowser(mdns.tcp('https'))
-  .on('serviceUp', mdnsServiceUp)
-  .on('serviceDown', mdnsServiceDown)
-  .on('error', mdnsError)
-  .start();
-};
-io.sockets.on('connection', socketConnect);
+
+ var io = socketio.listen(process.env.PORT || 3000);
+ //var io = socketio.listen(3000);
+ io.sockets.on('connection', socketConnect);
+} catch(ex) {
+ console.log('Exception = ' + ex);
+}
+
 
