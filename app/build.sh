7b98f248bc0dd02f1e1fa3a0674bb0fa3c701a29
Jason Kridner <jdk@ti.com>
2013-04-06 13:28:10 -0400

    app: created GREATLY simplified version

--- app/build.sh
+++ app/build.sh
@@ -6,24 +6,23 @@ cd $(dirname $0)/..
 DIR=`pwd`
 
 cd $DIR
-zip $ZIP -r autorun.inf Docs Drivers info.txt LICENSE.txt README.htm
+zip $ZIP -r autorun.inf Docs Drivers info.txt LICENSE.txt START.htm
 
 # Add package.json
 cd $DIR/app
-zip $ZIP -r package.json app.js index.html index.js node_modules jquery.js funcs.js
+zip $ZIP -r package.json
 
 # Make Mac app
 rm -f $DIR/app/beaglebone-getting-started.app.zip
 cp $ZIP $DIR/app/beaglebone-getting-started.app/Contents/Resources/app.nw
 zip $DIR/app/beaglebone-getting-started.app.zip -r $DIR/app/beaglebone-getting-started.app
 
-# Make Linux 64-bit app
-cat $DIR/app/node-webkit-latest-linux-x64/nw $ZIP > $DIR/app/beaglebone-getting-started.x64
-chmod +x $DIR/app/beaglebone-getting-started.x64
-
 # Make Linux 32-bit app
 cat $DIR/app/node-webkit-latest-linux-ia32/nw $ZIP > $DIR/app/beaglebone-getting-started.ia32
 chmod +x $DIR/app/beaglebone-getting-started.ia32
+
+# Make Windows .exe
+cat $DIR/app/node-webkit-latest-win-ia32/nw.exe $ZIP > $DIR/app/beaglebone-getting-started.exe
 
 # Clean-up
 rm -f $ZIP
