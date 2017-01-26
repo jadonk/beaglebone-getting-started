203eceaaf45231aff516678d1d31032063f054d7
Jason Kridner <jdk@ti.com>
2017-01-25 23:12:08 -0500

    Update Windows driver signature

--- Drivers/Windows/src/make.bat
+++ Drivers/Windows/src/make.bat
@@ -4,9 +4,9 @@ set ZIP_PATH="c:\Program Files\7-Zip"
 %WDK_PATH%\inf2cat /v /driver:%~dp0\files\FTDI /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,10_X86,10_X64
 %WDK_PATH%\inf2cat /v /driver:%~dp0\files\RNDIS /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,10_X86,10_X64
 %WDK_PATH%\inf2cat /v /driver:%~dp0\files\CDCACM /os:XP_X86,Vista_X86,Vista_X64,7_X86,7_X64,8_X86,8_X64,10_X86,10_X64
-%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" "files\FTDI\*.cat"
-%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" "files\RNDIS\*.cat"
-%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" "files\CDCACM\*.cat"
+%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a "files\FTDI\*.cat"
+%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a "files\RNDIS\*.cat"
+%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a "files\CDCACM\*.cat"
 %SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a dpinst\dpinst.exe 
 %SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a dpinst64\dpinst.exe 
 del BONE_DRV.7z
@@ -23,5 +23,5 @@ cd dpinst64
 cd ..
 copy /b 7zSD.sfx + config.txt + BONE_DRV.7z ..\BONE_DRV.exe
 copy /b 7zSD.sfx + config.txt + BONE_D64.7z ..\BONE_D64.exe
-%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" ..\BONE_DRV.exe 
-%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" ..\BONE_D64.exe 
+%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a ..\BONE_DRV.exe 
+%SDK_PATH%\signtool sign /v /n "BeagleBoard.org Foundation" /a ..\BONE_D64.exe 
