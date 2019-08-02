05bedba192646152b7bc80b0accaea75aef864e5
Jason Kridner <jdk@ti.com>
2019-08-02 07:35:25 -0400

    Create automated script for pulling https://beagleboard.org/getting-started

---
+++ scripts/get-latest-START.htm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+curl https://beagleboard.org/getting-started > START.htm
+perl -pe 's#(["\x27])/([^"\x27])#$1https://beagleboard.org/$2#g' -i START.htm
+perl -pe 's#(["\x27])https://beagleboard.org/static/Drivers/#$1Drivers/#g' -i START.htm
+perl -pe 's#(["\x27])https://beagleboard.org/static/#$1static/#g' -i START.htm
+perl -pe 's#<title>BeagleBoard.org - getting-started</title>#<title>Getting started with Beagle</title>#' -i START.htm
+perl -pe 's#href="/"#href="https://beagleboard.org/"#g' -i START.htm
