9c20bd1b2f50e9123464775a6a877c6e27ef81be
Jason Kridner <jdk@ti.com>
2019-08-02 08:40:25 -0400

    refresh

--- scripts/get-latest-START.htm.sh
+++ scripts/get-latest-START.htm.sh
@@ -5,4 +5,5 @@ perl -pe 's#(["\x27])https://beagleboard.org/static/Drivers/#$1Drivers/#g' -i ST
 perl -pe 's#(["\x27])https://beagleboard.org/static/#$1static/#g' -i START.htm
 perl -pe 's#<title>BeagleBoard.org - getting-started</title>#<title>Getting started with Beagle</title>#' -i START.htm
 perl -pe 's#href="/"#href="https://beagleboard.org/"#g' -i START.htm
+perl -pe 's#\r\n#\n#g' -i START.htm
 cp START.htm README.htm
